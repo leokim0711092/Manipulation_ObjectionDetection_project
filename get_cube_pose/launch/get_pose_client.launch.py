2758a4f79c7bc2d7c5412fe146a44c2144df9233
leokim0711092 <leokim10073@gmail.com>
2023-11-22 11:58:05 +0000

    Get pose node and rviz node in launch file

--- get_cube_pose/launch/get_pose_client.launch.py
+++ get_cube_pose/launch/get_pose_client.launch.py
@@ -1,8 +1,11 @@
 import launch
 from launch_ros.actions import Node
-
+from ament_index_python.packages import get_package_share_directory
+import os
 
 def generate_launch_description():
+
+    package_description = "get_cube_pose"
 
     basic_grasp = Node(
         package='simple_grasping',
@@ -13,8 +16,30 @@ def generate_launch_description():
         parameters=[{'debug_topics': True}]
         )
 
+    get_pose = Node(
+    package='get_cube_pose',
+    executable='get_pose_client',
+    name='get_pose_client',
+    output='screen',
+    emulate_tty=True
+    )
+
+    # RVIZ Configuration
+    rviz_config_dir = os.path.join(get_package_share_directory(package_description), 'rviz', 'perception.rviz')
+
+
+    rviz_node = Node(
+            package='rviz2',
+            executable='rviz2',
+            output='screen',
+            name='rviz_node',
+            parameters=[{'use_sim_time': True}],
+            arguments=['-d', rviz_config_dir])
+
     return launch.LaunchDescription(
     [
-        basic_grasp
+        basic_grasp,
+        get_pose,
+        rviz_node
     ]    
     )
